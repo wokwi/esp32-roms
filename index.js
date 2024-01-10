1a853452dc8dd6d493f2312f1631773bcd95bd00
Uri Shaked <uri@urishaked.com>
2024-01-10 15:35:46 +0200

    feat: add ESP32P4 ROM (preview)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ export const roms = {
   'esp32-c3': 'esp32c3-rom.bin',
   'esp32-c6': 'esp32c6-rom.bin',
   'esp32-h2': 'esp32h2-rom.bin',
+  'esp32-p4': 'esp32p4-rom-preview.bin',
 };
 
 export const symbols = {
@@ -14,4 +15,5 @@ export const symbols = {
   'esp32-c3': 'esp32c3-rom.sym',
   'esp32-c6': 'esp32c6-rom.sym',
   'esp32-h2': 'esp32h2-rom.sym',
+  'esp32-p4': 'esp32p4-rom-preview.sym',
 };
