b0e85f39478cf0be2b86a1637d72f0bfd48e50aa
Uri Shaked <uri@urishaked.com>
2025-12-10 19:44:41 +0200

    feat: esp32c5 rom (eco2)

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ export const roms: {
     'esp32-s2': string;
     'esp32-s3': string;
     'esp32-c3': string;
+    'esp32-c5': string;
     'esp32-c6': string;
     'esp32-h2': string;
     'esp32-p4': string;
@@ -12,6 +13,7 @@ export const symbols: {
     'esp32-s2': string;
     'esp32-s3': string;
     'esp32-c3': string;
+    'esp32-c5': string;
     'esp32-c6': string;
     'esp32-h2': string;
     'esp32-p4': string;
